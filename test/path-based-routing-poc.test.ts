96baa617e61f947b8f44e0db1a362a8ab73c844d
Greg Macbean <greg.macbean@gmail.com>
2022-05-27 15:07:54 +0930

    initialise cdk

---
+++ test/path-based-routing-poc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PathBasedRoutingPoc from '../lib/path-based-routing-poc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/path-based-routing-poc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PathBasedRoutingPoc.PathBasedRoutingPocStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
