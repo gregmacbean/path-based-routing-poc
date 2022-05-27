5cac649835c093c5ef850c67da28aa5583114c22
Greg Macbean <greg.macbean@gmail.com>
2022-05-27 15:49:29 +0930

    adding hello-world

---
+++ apps/hello-world/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <HelloWorld msg="Welcome to hello world"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+  background-color: blue;
+}
+</style>
